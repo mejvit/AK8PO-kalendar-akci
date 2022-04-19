466514e4e3ed9d7bd29ad3f86de45bb6ce8e425f
mejvit <mejvit@gmail.com>
2022-04-19 20:15:52 +0200

    Configuring proxy from BE to FE

--- MejvitCalendarBackend/Program.cs
+++ MejvitCalendarBackend/Program.cs
@@ -9,13 +9,22 @@ var connectionString = builder.Configuration.GetConnectionString("MejvitCalendar
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
-
+builder.Services.AddSpaStaticFiles(options => { options.RootPath = "wwwroot"; });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSpaStaticFiles();
+app.UseSpa(builder =>
+{
+    if (app.Environment.IsDevelopment())
+    {
+        builder.UseProxyToSpaDevelopmentServer("http://localhost:8081/");
+    }
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
